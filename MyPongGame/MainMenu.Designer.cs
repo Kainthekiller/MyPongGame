d99ff6def562b72da3c731b490f7a0669921333e
Philip Yates <PKYates@student.fullsail.edu>
2021-04-04 13:32:50 -0500

    Pong Game Menu and Close Created #Framework

---
+++ MyPongGame/MainMenu.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace MyPongGame
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PongTitleLabel = new System.Windows.Forms.Label();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PongTitleLabel
+            // 
+            this.PongTitleLabel.AutoSize = true;
+            this.PongTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PongTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PongTitleLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.PongTitleLabel.Location = new System.Drawing.Point(346, 50);
+            this.PongTitleLabel.Name = "PongTitleLabel";
+            this.PongTitleLabel.Size = new System.Drawing.Size(225, 73);
+            this.PongTitleLabel.TabIndex = 0;
+            this.PongTitleLabel.Text = "PONG";
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.BackColor = System.Drawing.Color.Transparent;
+            this.StartBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartBtn.ForeColor = System.Drawing.Color.Green;
+            this.StartBtn.Location = new System.Drawing.Point(382, 214);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(130, 66);
+            this.StartBtn.TabIndex = 1;
+            this.StartBtn.Text = "START";
+            this.StartBtn.UseVisualStyleBackColor = false;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(928, 574);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.PongTitleLabel);
+            this.Name = "MainMenu";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PongTitleLabel;
+        private System.Windows.Forms.Button StartBtn;
+    }
+}
+
---
+++ MyPongGame/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPongGame
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        //MainMenu Start Button
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            //Main Game Menu is Shown and MainMenu Is hidden
+            Game gameWindow = new Game();
+            gameWindow.Show();
+            this.Hide();
+        }
+    }
+}
